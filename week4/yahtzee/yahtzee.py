cc6da32d247250edfe308e8ca5572d19957cce7e
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2022-04-12 14:53:24 -0500

    Complete 'Yahtzee' assignment

---
+++ week4/yahtzee/yahtzee.py
@@ -0,0 +1,43 @@
+import random
+rolledDie = []
+
+class Die:
+    def __init__(self, sides, currentFace):
+        self.sides = sides
+        self.currentFace = currentFace
+    
+    def roll(self):
+        self.getCurrentFaceValue(random.randint(1, self.sides))
+
+    def getCurrentFaceValue(self, roll):
+        self.currentFace = roll
+
+    def showDieFace(self):
+        return self.currentFace
+
+def rollForYahtzee():
+    ctrlList = [1, 2, 3, 4, 5]
+    yahtzee = False
+
+    for i in ctrlList:
+        globals()[f'die{i}'] = Die(6, 1)
+    
+    for i in ctrlList:
+        globals()[f'die{i}'].roll()
+        rolledDie.append(globals()[f'die{i}'].showDieFace())
+
+    for rolls in rolledDie:
+        if rolledDie[0] != rolls:
+            yahtzee = False
+            break
+        else:
+            yahtzee = True
+
+    if yahtzee == False:
+        print(rolledDie)
+        print('Sorry no Yahtzee yet...')
+    else:
+        print(rolledDie)
+        print('YAHTZEE!')
+
+rollForYahtzee()
